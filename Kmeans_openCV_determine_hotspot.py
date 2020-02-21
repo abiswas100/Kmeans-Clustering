05ad0319980a6977719563f01e7d46593aec1d9a
Avhishek Biswas <avhishekbiswas17@gmail.com>
2020-02-21 17:43:56 -0600

    Update Kmeans_openCV_determine_hotspot.py

--- Kmeans_openCV_determine_hotspot.py
+++ Kmeans_openCV_determine_hotspot.py
@@ -18,29 +18,35 @@ for files in os.listdir():
         img = cv2.imread(str(files))
         image_list.append(img)
         filename.append(files)
-        print("works....")
+        print("All Images loaded into array")
 
-image = cv2.imread("F:\Kmeans\Museum_Tryout_12.jpg")
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-# reshape the image to a 2D array of pixels and 3 color values (RGB)
-pixel_values = image.reshape((-1, 3))
-# convert to float
-pixel_values = np.float32(pixel_values)
-print(pixel_values)
-len(pixel_values)
+#Restricting python to use only 2 cores
+cpu_nums = list(range(psutil.cpu_count()))
+proc = psutil.Process(os.getpid())
+proc.cpu_affinity(cpu_nums[:2])
+print("CPUS being consumed..",cpu_count())
 
+
+counter = 0
+for image in image_list:
+    counter = counter +1 
+    #plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # reshape the image to a 2D array of pixels and 3 color values (RGB)
+    pixel_values = image.reshape((-1, 3))
+    # convert to float
+    pixel_values = np.float32(pixel_values)
+    print(pixel_values)
+    len(pixel_values)
 
 #create an array for the number of clusters
 n_clusters = 6
 averages = []
-
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.1)
 start = time.time()
 compactness, labels, centers = cv2.kmeans(pixel_values, n_clusters, None, criteria, 10, cv2.KMEANS_PP_CENTERS)
 end = time.time()
 print("Time consumed in working: ",end - start)
-
 # convert back to 8 bit values
 centers = np.uint8(centers)
 print(centers)
@@ -51,9 +57,6 @@ print(set(labels))
 print(len(labels))
 
 segmented_image = centers[labels]
-segmented_image
-
-
 # reshape back to the original image dimension
 segmented_image = segmented_image.reshape(image.shape)
 # show the image
@@ -64,16 +67,10 @@ plt.imshow(cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB))
 masked_image = np.copy(image)
 # convert to the shape of a vector of pixel values
 masked_image = masked_image.reshape((-1, 3))
-#masked_image
-
-
 # color (i.e cluster) to disable
 clusters = [0,1,2,3,4,5]
-
-
 #for x in clusters:
 temp = np.array()    #temp array that has the pixel values for a specific cluster
-
 temp = masked_image[labels == 0]
 # print("\ncluster : ",x )""
 temp=temp.flatten()
@@ -87,6 +84,7 @@ masked_image[labels == 2] = [255,255,255]   #hotspot for 12 is cluster 2
 masked_image = masked_image.reshape(image.shape)
 # show the image
 plt.imshow(cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB))
+
 
 
 
