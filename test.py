e3cd76e62b28c8245596c01064b5c0202cb19fa5
Avhishek Biswas <avhishekbiswas17@gmail.com>
2020-02-24 14:36:17 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,38 @@
-if __name__=='__main__':
-    print("This is run as a main file")
-else:
-    print("this is a module")
+import re
+import csv
+
+#this function works just fine we can use this
+def extract_temperature(csv_filename):
+    '''
+    Function will extract all of the temperatures within a csv file into a 2D list of lenth [512][640].
+    The temperature values should be in celisus and the 2D list cannot be any smaller than this.
+    :param csv_filename: CSV filename you you are trying to parse
+    :param dir_path: Parent directory to OGI_u_value_module project
+    :param csv_file_path: CSV filepath extension
+    :return: List containing [512][640] = 327680 data points
+    '''
+
+    csv_name = re.split(r'[_.]', csv_filename)
+    if('MWIR' in csv_name):
+        with open(csv_filename) as csv_file:
+            csv_file_content = csv.reader(csv_file, delimiter=',', quotechar='|')
+            pixel_temperature = []
+
+            for i, data in enumerate(csv_file_content):
+                if(i >= 8):
+                    pixel_temperature.append(data[0:])
+    else:
+        with open(csv_filename) as csv_file:
+            csv_file_content = csv.reader(csv_file, delimiter=',', quotechar='|')
+            pixel_temperature = []
+
+            for i, data in enumerate(csv_file_content):
+                if(i >= 1):
+                    pixel_temperature.append(data[0:])
+
+    return pixel_temperature
+
+temperature = extract_temperature()
+ 
+def calculate_temperature():
+    pass
