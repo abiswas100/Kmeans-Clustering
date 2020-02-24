3e3bb276e57ac784754033665174b13e527b05ec
Sam <sdressler318@gmail.com>
2020-02-23 22:00:33 -0600

    progress...

--- Kmeans_Scikt-learn_determine_hotspot.py
+++ Kmeans_Scikt-learn_determine_hotspot.py
@@ -1,19 +1,19 @@
 import os
 import time
-from multiprocessing import cpu_count
-
+import csv
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import psutil
+import matplotlib.pyplot as plt
+from multiprocessing import cpu_count
 from progressbar import ProgressBar
 from sklearn.cluster import KMeans
 
 image_list = []
 filename = []
 counter = 0  
-os.chdir(r"Museum Clustering Tryouts//images")
+os.chdir(r"D:\UND\GCSP\Project_Files\Kmeans\Museum Clustering Tryouts\images")
 for files in os.listdir():
     if(files.endswith('.jpg')):
         if(counter == 0):   # to input all the image just remove the conditional statements and use the below 4 lines
@@ -36,8 +36,8 @@ pbar = ProgressBar()
 start = time.time()
 clustered_images_list = [] #list containing all the clustered outputs
  # Running 6 clusters on each image of Museum
- # For Twamley keep cluster above 10 iam
-
+ # For Twamley keep cluster above 10 
+print("")
 print("Clustering the dataset in into ")
 for image in pbar(image_list):
     # reshape the image to a 2D array of pixels and 3 color values (RGB)
@@ -54,7 +54,7 @@ for image in pbar(image_list):
     print(centers)
     # flatten the labels array
     labels = kmeans.labels_
-    # print(labels)
+    print("The actual labels array",labels)
     print("The labels set",set(labels),"The length of the labels array",len(labels))
     segmented_image = centers[labels]
     segmented_image = segmented_image.reshape(image.shape)
@@ -62,41 +62,68 @@ for image in pbar(image_list):
 
 end = time.time()
 print("Time consumed in working: ",end - start)
+##########################################
+#           Find Average Temperature
+##########################################
+bool_matrix = np.zeros(len(labels))
+print(len(labels))
+clusters = [0,1,2,3,4,5];
+masked_image = np.copy(image)
+# convert to the shape of a vector of pixel values
+masked_image = masked_image.reshape((-1, 3))
+for i in clusters:
+    #check each index for every label
+        masked_image[labels == i] 
+        print(i)
+#masked_image[labels == 2] = [255,255,255]
+masked_image = masked_image.reshape(image.shape)
 
 
 
 
 
+############################################
+#the labels array contains all the pixel ..it is of size 32780 so we have to interpret the a 1-D labels array 
+# as pixels in x,y co-ordinate.
+
+#Saving the images in output folder
+
+try:
+    os.mkdir('kmeans-output')
+except FileExistsError:
+    print("File already exists so just saving them in that folder")
+    os.removedirs('Kmeans-output')
+    os.mkdir('kmeans-output')
+    print("")
+print("Pushing clustered images to disk..............")    
+os.chdir('kmeans-output')
+counter = 0
+for img in clustered_images_list:
+    # show the image
+    #plt.imshow(cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB))
+    #cv2.imwrite(str(counter) + '.jpg', img)
+    cv2.imwrite(filename[0], img)
+    counter = counter + 1
+print("Finished .................")
 
 
+def extract_temperature(csv_filename):
+    '''
+    Function will extract all of the temperatures within a csv file into a 2D list of lenth [512][640].
+    The temperature values should be in celisus and the 2D list cannot be any smaller than this.
+    :param csv_filename: CSV filename you you are trying to parse
+    :param dir_path: Parent directory to OGI_u_value_module project
+    :param csv_file_path: CSV filepath extension
+    :return: List containing [512][640] = 327680 data points
+    '''
+    with open(csv_filename) as csv_file:
 
-
-
-
-# # disable only the cluster number 2 (turn the pixel into black)
-# masked_image = np.copy(image)
-# # convert to the shape of a vector of pixel values
-# masked_image = masked_image.reshape((-1, 3))
-# # color (i.e cluster) to disable
-# clusters = [0,1,2,3,4,5]
-# #for x in clusters:
-# temp = np.array()    #temp array that has the pixel values for a specific cluster
-# temp = masked_image[labels == 0]
-# # print("\ncluster : ",x )""
-# temp=temp.flatten()
-# print(len(temp))
-# print(temp)
-
-# # convert back to original shape
-# for i in clusters:
-#     masked_image[labels == i] = [0,0,0]
-# masked_image[labels == 2] = [255,255,255]   #hotspot for 12 is cluster 2
-# masked_image = masked_image.reshape(image.shape)
-# # show the image
-# plt.imshow(cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB))
-
-
-
-
-
-averages = []
+        csv_file_content = csv.reader(csv_file, delimiter=',', quotechar='|')
+        pixel_temperature = []
+        for i, data in enumerate(csv_file_content):
+            length = len(data)
+            if(length == 0):
+                continue
+            if(i <= 8):
+                pixel_temperature.append(data[0:])
+    return pixel_temperature
