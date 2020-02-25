0927c73ae07fee52511c546902ab79b4be9ab3c8
Avhishek Biswas <avhishekbiswas17@gmail.com>
2020-02-24 18:53:13 -0600

    Update Kmeans_Scikt-learn_determine_hotspot.py

--- Kmeans_Scikt-learn_determine_hotspot.py
+++ Kmeans_Scikt-learn_determine_hotspot.py
@@ -1,6 +1,8 @@
-from .optimum_k import silhoette
+import optimum_K as op
+import Find_best_cluster as fb
 
 import os
+import shutil as s
 import time
 from multiprocessing import cpu_count
 
@@ -40,16 +42,21 @@ clustered_images_list = [] #list containing all the clustered outputs
  # Running 6 clusters on each image of Museum
  # For Twamley keep cluster above 10 
 print("")
-print("Clustering the dataset in into ")
+print("Clustering the image ")
+labels_of_all_image = []
 for image in pbar(image_list):
     # reshape the image to a 2D array of pixels and 3 color values (RGB)
     pixel_values = image.reshape((-1, 3))
     # convert to float
     pixel_values = np.float32(pixel_values)
+    print("")
+    print("The pixel array is ")
     print(pixel_values)
     print("Length of the pixel value list",len(pixel_values))
+    print("")
+    #this function will return the best k for each image
     
-    silhoette(pixel_values)
+    # cluster = op.silhoette(pixel_values)
     
     #create an array for the number of clusters
     kmeans = KMeans(n_clusters=6, random_state=0, n_jobs = -1).fit(pixel_values)
@@ -59,62 +66,49 @@ for image in pbar(image_list):
     print("The centers are ----",centers)
     # flatten the labels array
     labels = kmeans.labels_
+    labels_of_all_image.append(labels)
     print("The actual labels array",labels)
-    print("The labels set",set(labels),"The length of the labels array",len(labels))
+    print("The labels set - ",set(labels),"   The length of the labels array",len(labels))
     segmented_image = centers[labels]
     segmented_image = segmented_image.reshape(image.shape)
     clustered_images_list.append(segmented_image)
 
+print(" ")
 end = time.time()
 print("Time consumed in working: ",end - start)
 
+#function to mask only the hotspot
+# masked_image_list = []
 
-#the labels array contains all the pixel ..it is of size 32780 so we have to interpret the a 1-D labels array 
-# as pixels in x,y co-ordinate.
-
-
-
-
-
-
-# # disable only the cluster number 2 (turn the pixel into black)
-# masked_image = np.copy(image)
-# # convert to the shape of a vector of pixel values
-# masked_image = masked_image.reshape((-1, 3))
-# # color (i.e cluster) to disable
-# clusters = [0,1,2,3,4,5]
-# #for x in clusters:
-# temp = np.array()    #temp array that has the pixel values for a specific cluster
-# temp = masked_image[labels == 0]
-# # print("\ncluster : ",x )""
-# temp=temp.flatten()
-# print(len(temp))
-# print(temp)
-
-# # convert back to original shape
-# for i in clusters:
-#     masked_image[labels == i] = [0,0,0]
-# masked_image[labels == 2] = [255,255,255]   #hotspot for 12 is cluster 2
-# masked_image = masked_image.reshape(image.shape)
-# # show the image
-# plt.imshow(cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB))
-
-
+# for img in clustered_images_list:
+#     masked_image = np.copy(img)
+#     # convert to the shape of a vector of pixel values
+#     masked_image = masked_image.reshape((-1, 3))
+#     index_of_image = clustered_images_list.index(img)
+#     best_cluster = fb.calculate_temperature(labels_of_all_image[index_of_image],filename[index_of_image])
+#     for i in range(0,6):
+#         if i == best_cluster:
+#             masked_image[labels == best_cluster] = [255,255,255]      
+#         else:
+#             masked_image[labels == i] = [0,0,0]
+#     masked_image_list.append(masked_image)        
+    
 #Saving the images in output folder
+print("  ")
 try:
     os.mkdir('kmeans-output')
 except FileExistsError:
-    print("File already exists so just saving them in that folder")
-    os.removedirs('Kmeans-output')
+    print(" ")
+    print("Folder already exists so removing the previous outputs and creating again")
+    s.rmtree('kmeans-output')
     os.mkdir('kmeans-output')
-    print("")
-print("Pushing clustered images to disk..............")    
-os.chdir('kmeans-output')
-counter = 0
-for img in clustered_images_list:
-    # show the image
-    #plt.imshow(cv2.cvtColor(segmented_image, cv2.COLOR_BGR2RGB))
-    #cv2.imwrite(str(counter) + '.jpg', img)
-    cv2.imwrite(filename[0], img)
-    counter = counter + 1
+    print(" ")
+finally:
+    print("Pushing clustered images to disk..............")    
+    os.chdir('kmeans-output')
+    counter = 0
+    for img in clustered_images_list:
+        print(" ")
+        cv2.imwrite(filename[0], img)
+        counter = counter + 1
 print("Finished .................")
