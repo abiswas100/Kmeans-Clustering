e4b99308bc59f8735bddbc46108c925a1fefe491
Avhishek Biswas <avhishekbiswas17@gmail.com>
2020-02-28 11:48:05 -0600

    Update Kmeans_Scikt-learn_determine_hotspot.py

--- Kmeans_Scikt-learn_determine_hotspot.py
+++ Kmeans_Scikt-learn_determine_hotspot.py
@@ -74,6 +74,7 @@ def start_parsing(json_files, project_name):
         for files in os.listdir():
             if(files.endswith('.jpg')):
                 img = cv2.imread(str(files))
+                filenames.append(files)
         new_list = []
         for i in range(len(x_coordinates)):
             print("{} {}".format(x_coordinates[i], y_coordinates[i]))
@@ -82,7 +83,7 @@ def start_parsing(json_files, project_name):
             new_list.append([r,g,b])
             
             
-        print(len(new_list))
+        print(len(new_list)),filenames
         
 # start_parsing("test", "test")
 #         for i in len(x_coordiantes):
@@ -91,6 +92,8 @@ def start_parsing(json_files, project_name):
 #     #         counter = counter+1
 #     # else:            
 #     #           break
+
+new_image_list,filenames = start_parsing()
 #Restricting python to use only 2 cores
 cpu_nums = list(range(psutil.cpu_count()))
 proc = psutil.Process(os.getpid())
@@ -106,7 +109,7 @@ clustered_images_list = [] #list containing all the clustered outputs
 print("")
 print("Clustering the image ")
 labels_of_all_image = []
-for image in pbar(new_list):
+for image in pbar(new_image_list):
     # reshape the image to a 2D array of pixels and 3 color values (RGB)
     pixel_values = image.reshape((-1, 3))
     # convert to float
@@ -153,7 +156,7 @@ for image in clustered_images_list:
     # convert to the shape of a vector of pixel values
     masked_image = masked_image.reshape((-1, 3))
     # index_of_image = clustered_images_list.index(image)
-    best_cluster, no_of_labels = fb.calculate_temperature(labels_of_all_image[counter],filename[counter])
+    best_cluster, no_of_labels = fb.calculate_temperature(labels_of_all_image[counter],filenames[counter])
     labels = labels_of_all_image[counter]
     for i in range(0,6):
         if i == best_cluster:
@@ -163,8 +166,81 @@ for image in clustered_images_list:
     masked_image = masked_image.reshape(image.shape)
     masked_image_list.append(masked_image)     
     counter = counter+1  
-    density of pixels = 327800/
 #Saving the masked images in Kmeans-masked-output folder
+print("  ")
+try:
+    path = os.getcwd()
+    parent_path = Path(path).parent
+    os.chdir(parent_path)    
+    os.mkdir('kmeans-output')
+
+except FileExistsError:
+    print(" ")
+    print("Folder already exists so removing the previous outputs and creating again")
+    s.rmtree('kmeans-output')
+    os.mkdir('kmeans-output')
+
+finally:
+    print("Pushing clustered images to disk..............")    
+    os.chdir('kmeans-output')
+    counter = 0
+    for img in masked_image_list:
+        cv2.imwrite(filenames[counter], img)
+        counter = counter + 1
+        img = 0
+print("Finished .................")
+print(" ")
+print(" ")
+
+
+
+# #Saving the clustered images in output folder
+
+#     #create an array for the number of clusters
+#     kmeans = KMeans(n_clusters=6, random_state=0, n_jobs = -1).fit(pixel_values)
+#     # convert back to 8 bit values
+#     centers = kmeans.cluster_centers_
+#     centers = np.uint8(centers)
+#     # print("The centers are ----",centers)
+#     # flatten the labels array
+#     labels = kmeans.labels_
+#     labels_of_all_image.append(labels)
+#     # print("The actual labels array",labels)
+#     # print("The labels set - ",set(labels),"   The length of the labels array",len(labels))
+#     segmented_image = centers[labels]
+#     segmented_image = segmented_image.reshape(image.shape)
+#     clustered_images_list.append(segmented_image)
+
+# print(" ")
+# end = time.time()
+# print("Time consumed in clustering: ",end - start)
+
+
+#function to mask only the hotspot
+print("")
+print("Masking the image finding the best cluster")
+masked_image_list = []
+print("")
+counter = 0
+for image in clustered_images_list:
+    masked_image = 0
+    masked_image = np.copy(image)
+    # convert to the shape of a vector of pixel values
+    masked_image = masked_image.reshape((-1, 3))
+    # index_of_image = clustered_images_list.index(image)
+    best_cluster = fb.calculate_temperature(labels_of_all_image[counter],filename[counter])
+    labels = labels_of_all_image[counter]
+    for i in range(0,6):
+        if i == best_cluster:
+            masked_image[labels == best_cluster] = [255,255,255]      
+        # else:
+        #     masked_image[labels == i] = [0,0,0]
+    masked_image = masked_image.reshape(image.shape)
+    masked_image_list.append(masked_image)     
+    counter = counter+1  
+
+# #Saving the masked images in Kmeans-masked-output folder
+
 print("  ")
 try:
     path = os.getcwd()
@@ -189,80 +265,6 @@ finally:
 print("Finished .................")
 print(" ")
 print(" ")
-
-
-
-# #Saving the clustered images in output folder
-=======
-#     #create an array for the number of clusters
-#     kmeans = KMeans(n_clusters=6, random_state=0, n_jobs = -1).fit(pixel_values)
-#     # convert back to 8 bit values
-#     centers = kmeans.cluster_centers_
-#     centers = np.uint8(centers)
-#     # print("The centers are ----",centers)
-#     # flatten the labels array
-#     labels = kmeans.labels_
-#     labels_of_all_image.append(labels)
-#     # print("The actual labels array",labels)
-#     # print("The labels set - ",set(labels),"   The length of the labels array",len(labels))
-#     segmented_image = centers[labels]
-#     segmented_image = segmented_image.reshape(image.shape)
-#     clustered_images_list.append(segmented_image)
-
-# print(" ")
-# end = time.time()
-# print("Time consumed in clustering: ",end - start)
-
-
-# #function to mask only the hotspot
-# print("")
-# print("Masking the image finding the best cluster")
-# masked_image_list = []
-# print("")
-# counter = 0
-# for image in clustered_images_list:
-#     masked_image = 0
-#     masked_image = np.copy(image)
-#     # convert to the shape of a vector of pixel values
-#     masked_image = masked_image.reshape((-1, 3))
-#     # index_of_image = clustered_images_list.index(image)
-#     best_cluster = fb.calculate_temperature(labels_of_all_image[counter],filename[counter])
-#     labels = labels_of_all_image[counter]
-#     for i in range(0,6):
-#         if i == best_cluster:
-#             masked_image[labels == best_cluster] = [255,255,255]      
-#         # else:
-#         #     masked_image[labels == i] = [0,0,0]
-#     masked_image = masked_image.reshape(image.shape)
-#     masked_image_list.append(masked_image)     
-#     counter = counter+1  
-
-# #Saving the masked images in Kmeans-masked-output folder
->>>>>>> Stashed changes
-# print("  ")
-# try:
-#     path = os.getcwd()
-#     parent_path = Path(path).parent
-#     os.chdir(parent_path)    
-#     os.mkdir('kmeans-output')
-
-# except FileExistsError:
-#     print(" ")
-#     print("Folder already exists so removing the previous outputs and creating again")
-#     s.rmtree('kmeans-output')
-#     os.mkdir('kmeans-output')
-
-# finally:
-#     print("Pushing clustered images to disk..............")    
-#     os.chdir('kmeans-output')
-#     counter = 0
-#     for img in masked_image_list:
-#         cv2.imwrite(filename[counter], img)
-#         counter = counter + 1
-#         img = 0
-# print("Finished .................")
-# print(" ")
-# print(" ")
 
 
 
